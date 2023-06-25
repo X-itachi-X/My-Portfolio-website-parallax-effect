d79c7cc255d0a1e545b7f0fb3d2f67b9006cbc9d
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-25 11:17:24 +0000

    Jun 25, 2023, 4:47 PM

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ window.addEventListener("mousemove", (e) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
         let speedz = el.dataset.speedz;
-        let rotationSpeed = el.dataset.rotateDegree
+        let rotationSpeed = el.dataset.rotate;
         
         let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
         let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft *0.1;
