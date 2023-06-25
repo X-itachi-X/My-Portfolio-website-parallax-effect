3131b90c14a9972ac3d4563398e0a0f0b51460b6
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-25 10:53:01 +0000

    Jun 25, 2023, 4:23 PM

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue=0 , yValue=0 ;
-let rotateDegree =
+let rotateDegree = 0;
 
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
