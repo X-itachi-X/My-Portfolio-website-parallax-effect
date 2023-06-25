be76685436ede278a405f418fbf5ada1c68b2cc5
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-25 09:48:59 +0000

    Jun 25, 2023, 3:18 PM

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const parallax_el = document.querySelectorAll(".parallax");
 let xValue=0 , yValue=0 ;
 
 window.addEventListener("mousemove", (e) => {
-    xValue = e.clientX - win;
+    xValue = e.clientX - window.innerWidth;
     yValue = e.clientY;
 
     console.log(xValue,yValue)
