32cacb76cb92a20c98396ccb2f7a8b52a21b8142
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-25 10:26:09 +0000

    Jun 25, 2023, 3:56 PM

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ window.addEventListener("mousemove", (e) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
 
-        let zValue = 100;
+        let zValue = 1000;
         el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
     })
 })
