0240e1cdfba06f7b349bcbce9ea3da4fc4023a56
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-25 10:53:53 +0000

    Jun 25, 2023, 4:23 PM

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let rotateDegree = 0;
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
-
+    rotateDegree = xValue / (window.innerWidth / 2)
     parallax_el.forEach(el => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
